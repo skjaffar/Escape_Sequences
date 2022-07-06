d93ce2014cbcec03cabb14a03d75854a4deaf257
Shaik Jaffar <107207838+skjaffar@users.noreply.github.com>
2022-07-06 13:47:25 +0530

    Add files via upload

---
+++ Escape_Sequence.py.py
@@ -0,0 +1,34 @@
+# print('my\afriend')  # \a alest
+# print('my\\afriend') # my\afriend
+
+# print('my\bfriend')  #  back space 
+# print('my\\bfriend')
+
+# print('my\fworld')
+# print('my\\fworld')
+
+# print('my\nworld')  #  for new line
+# print('my\\nworld')  # print back \ 
+
+# print('my\rworld') # my\rworld
+# print('hello\rfriend') 
+
+# print('my\ufriend') # unicode error have to show 
+# print('my\xfriend')  #  in x should have to show unicode error 
+
+# print('my\tfriend')
+# print('\tmyfriend')
+# print('my\t   friend')
+
+
+
+
+
+
+
+
+
+
+
+
+
